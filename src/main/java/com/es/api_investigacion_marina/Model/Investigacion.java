6437cd6c2db61c2480dd1714d4b871ba9e825f15
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2024-12-10 20:12:18 +0100

    Models Completados

--- src/main/java/com/es/api_investigacion_marina/Model/Investigacion.java
+++ src/main/java/com/es/api_investigacion_marina/Model/Investigacion.java
@@ -1,4 +1,125 @@
 package com.es.api_investigacion_marina.Model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "investigaciones")
 public class Investigacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_investigacion")
+    private Long idInvestigacion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_investigador")
+    private Long idInvestigador;
+
+    @OneToMany (mappedBy = "investigaciones", fetch = FetchType.EAGER)
+    private List<Pez> peces;
+
+    private String titulo;
+
+    private String resumen;
+
+    private String lugar;
+
+    private LocalDate fecha;
+
+    private LocalTime hora;
+
+    // Constructores
+
+    public Investigacion(Long idInvestigacion, Long idInvestigador, List<Pez> peces, String titulo, String resumen, String lugar, LocalDate fecha, LocalTime hora) {
+        this.idInvestigacion = idInvestigacion;
+        this.idInvestigador = idInvestigador;
+        this.peces = peces;
+        this.titulo = titulo;
+        this.resumen = resumen;
+        this.lugar = lugar;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public Investigacion(Long idInvestigador, List<Pez> peces, String titulo, String resumen, String lugar, LocalDate fecha, LocalTime hora) {
+        this.idInvestigador = idInvestigador;
+        this.peces = peces;
+        this.titulo = titulo;
+        this.resumen = resumen;
+        this.lugar = lugar;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public Investigacion() {}
+
+    // Getters y Setters
+
+    public Long getIdInvestigacion() {
+        return idInvestigacion;
+    }
+
+    public void setIdInvestigacion(Long idInvestigacion) {
+        this.idInvestigacion = idInvestigacion;
+    }
+
+    public Long getIdInvestigador() {
+        return idInvestigador;
+    }
+
+    public void setIdInvestigador(Long idInvestigador) {
+        this.idInvestigador = idInvestigador;
+    }
+
+    public List<Pez> getPeces() {
+        return peces;
+    }
+
+    public void setPeces(List<Pez> peces) {
+        this.peces = peces;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
 }
--- src/main/java/com/es/api_investigacion_marina/Model/Pez.java
+++ src/main/java/com/es/api_investigacion_marina/Model/Pez.java
@@ -1,4 +1,148 @@
 package com.es.api_investigacion_marina.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "peces")
 public class Pez {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pez")
+    private Long idPez;
+
+    @ManyToOne
+    @JoinColumn(name = "id_investigador")
+    private Long idInvestigador;
+
+    @Column(name = "nombre_comun")
+    private String nombreComun;
+
+    @Column(name = "nombre_cientifico")
+    private String nombreCientifico;
+
+    private String especie;
+
+    private String dieta;
+
+    private String descripcion;
+
+    private int ejemplares;
+
+    @Column(name = "tamaño_maximo")
+    private Double tamMax;
+
+    @Column(name = "peligro_extincion")
+    private boolean peligroExtincion;
+
+    // Constructores
+
+    public Pez(Long idPez, Long idInvestigador, String nombreComun, String nombreCientifico, String especie, String dieta, String descripcion, int ejemplares, Double tamMax, boolean peligroExtincion) {
+        this.idPez = idPez;
+        this.idInvestigador = idInvestigador;
+        this.nombreComun = nombreComun;
+        this.nombreCientifico = nombreCientifico;
+        this.especie = especie;
+        this.dieta = dieta;
+        this.descripcion = descripcion;
+        this.ejemplares = ejemplares;
+        this.tamMax = tamMax;
+        this.peligroExtincion = peligroExtincion;
+    }
+
+    public Pez(Long idInvestigador, String nombreComun, String nombreCientifico, String especie, String dieta, String descripcion, int ejemplares, Double tamMax, boolean peligroExtincion) {
+        this.idInvestigador = idInvestigador;
+        this.nombreComun = nombreComun;
+        this.nombreCientifico = nombreCientifico;
+        this.especie = especie;
+        this.dieta = dieta;
+        this.descripcion = descripcion;
+        this.ejemplares = ejemplares;
+        this.tamMax = tamMax;
+        this.peligroExtincion = peligroExtincion;
+    }
+
+    public Pez() {}
+
+    // Getters y Setters
+
+    public Long getIdPez() {
+        return idPez;
+    }
+
+    public void setIdPez(Long idPez) {
+        this.idPez = idPez;
+    }
+
+    public Long getIdInvestigador() {
+        return idInvestigador;
+    }
+
+    public void setIdInvestigador(Long idInvestigador) {
+        this.idInvestigador = idInvestigador;
+    }
+
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getDieta() {
+        return dieta;
+    }
+
+    public void setDieta(String dieta) {
+        this.dieta = dieta;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getEjemplares() {
+        return ejemplares;
+    }
+
+    public void setEjemplares(int ejemplares) {
+        this.ejemplares = ejemplares;
+    }
+
+    public Double getTamMax() {
+        return tamMax;
+    }
+
+    public void setTamMax(Double tamMax) {
+        this.tamMax = tamMax;
+    }
+
+    public boolean isPeligroExtincion() {
+        return peligroExtincion;
+    }
+
+    public void setPeligroExtincion(boolean peligroExtincion) {
+        this.peligroExtincion = peligroExtincion;
+    }
 }
