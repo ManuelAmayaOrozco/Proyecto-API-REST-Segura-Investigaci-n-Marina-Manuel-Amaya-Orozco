f825703b88c40e9e9439e39d04f1dc400ab07bdd
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2024-12-11 19:14:42 +0100

    Repositorios Completos

--- src/main/java/com/es/api_investigacion_marina/Repository/InvestigacionRepository.java
+++ src/main/java/com/es/api_investigacion_marina/Repository/InvestigacionRepository.java
@@ -1,4 +1,8 @@
 package com.es.api_investigacion_marina.Repository;
 
-public interface InvestigacionRepository {
-}
+import com.es.api_investigacion_marina.Model.Investigacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvestigacionRepository extends JpaRepository<Investigacion, Long> {}
--- src/main/java/com/es/api_investigacion_marina/Repository/PezRepository.java
+++ src/main/java/com/es/api_investigacion_marina/Repository/PezRepository.java
@@ -1,4 +1,8 @@
 package com.es.api_investigacion_marina.Repository;
 
-public interface PezRepository {
-}
+import com.es.api_investigacion_marina.Model.Pez;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PezRepository extends JpaRepository<Pez, Long> {}
